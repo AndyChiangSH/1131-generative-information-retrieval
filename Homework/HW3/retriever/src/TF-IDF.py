28c9990e0fd5d549d764843796cd1a704771d8ef
AndyChiangSH <andy10801@gmail.com>
2024-12-19 19:20:57 +0800

    HW3: TF-IDF_13

--- Homework/HW3/retriever/src/TF-IDF.py
+++ Homework/HW3/retriever/src/TF-IDF.py
@@ -129,11 +129,11 @@ def retrieve(train_path, test_path, test_images_path, top_k=30):
 
 if __name__ == "__main__":
     CONFIG = {
-        "train_path": "image_captioning/dataset/base+blip-image-captioning-base_1/train.jsonl",
-        "test_path": "image_captioning/dataset/base+blip-image-captioning-base_1/test.jsonl",
-        "test_images_path": "image_captioning/dataset/base+blip-image-captioning-base_1/test_images.jsonl",
-        "submission_path": "retriever/submission/TF-IDF_12.csv",
-        "log_path": "retriever/log/TF-IDF_12.log"
+        "train_path": "image_captioning/dataset/base+blip-image-captioning-large_1/train.jsonl",
+        "test_path": "image_captioning/dataset/base+blip-image-captioning-large_1/test.jsonl",
+        "test_images_path": "image_captioning/dataset/base+blip-image-captioning-large_1/test_images.jsonl",
+        "submission_path": "retriever/submission/TF-IDF_13.csv",
+        "log_path": "retriever/log/TF-IDF_13.log"
     }
     
     logging.basicConfig(
