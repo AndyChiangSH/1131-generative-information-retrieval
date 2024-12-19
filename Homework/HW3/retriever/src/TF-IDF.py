0805b3f57f5892838a5e51d733e8b093ddb27dbc
AndyChiangSH <andy10801@gmail.com>
2024-12-19 15:20:14 +0800

    HW3: TF-IDF_3

--- Homework/HW3/retriever/src/TF-IDF.py
+++ Homework/HW3/retriever/src/TF-IDF.py
@@ -129,11 +129,11 @@ def retrieve(train_path, test_path, test_images_path, top_k=30):
 
 if __name__ == "__main__":
     CONFIG = {
-        "train_path": "dataset/train.jsonl",
-        "test_path": "dataset/test.jsonl",
-        "test_images_path": "dataset/test_images.jsonl",
-        "submission_path": "submission/TF-IDF_2.csv",
-        "log_path": "log/TF-IDF_2.log"
+        "train_path": "image_captioning/dataset/git-base_1/train.jsonl",
+        "test_path": "image_captioning/dataset/git-base_1/test.jsonl",
+        "test_images_path": "image_captioning/dataset/git-base_1/test_images.jsonl",
+        "submission_path": "retriever/submission/TF-IDF_3.csv",
+        "log_path": "retriever/log/TF-IDF_3.log"
     }
     
     logging.basicConfig(
