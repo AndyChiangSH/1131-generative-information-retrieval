0701b9f9e8ec182d0d0fe4b117afd7d26d3786b3
AndyChiangSH <andy10801@gmail.com>
2024-12-20 11:39:09 +0800

    HW3: TF-IDF_14

--- Homework/HW3/image_captioning/src/concat.py
+++ Homework/HW3/image_captioning/src/concat.py
@@ -33,9 +33,9 @@ def concat_descriptions(original_path, captioned_path, output_path):
 
 if __name__ == "__main__":
     CONFIG = {
-        "input_path": "dataset/",
-        "concat_path": "image_captioning/dataset/blip-image-captioning-large_1/",
-        "output_path": "image_captioning/dataset/base+blip-image-captioning-large_1/",
+        "input_path": "image_captioning/dataset/base+blip-image-captioning-large_1/",
+        "concat_path": "image_captioning/dataset/git-large-coco_1/",
+        "output_path": "image_captioning/dataset/base+blip-image-captioning-large_1+git-large-coco_1/",
     }
     
     logging.basicConfig(
--- Homework/HW3/retriever/src/TF-IDF.py
+++ Homework/HW3/retriever/src/TF-IDF.py
@@ -129,11 +129,11 @@ def retrieve(train_path, test_path, test_images_path, top_k=30):
 
 if __name__ == "__main__":
     CONFIG = {
-        "train_path": "image_captioning/dataset/base+blip-image-captioning-large_1/train.jsonl",
-        "test_path": "image_captioning/dataset/base+blip-image-captioning-large_1/test.jsonl",
-        "test_images_path": "image_captioning/dataset/base+blip-image-captioning-large_1/test_images.jsonl",
-        "submission_path": "retriever/submission/TF-IDF_13.csv",
-        "log_path": "retriever/log/TF-IDF_13.log"
+        "train_path": "image_captioning/dataset/base+blip-image-captioning-large_1+git-large-coco_1/train.jsonl",
+        "test_path": "image_captioning/dataset/base+blip-image-captioning-large_1+git-large-coco_1/test.jsonl",
+        "test_images_path": "image_captioning/dataset/base+blip-image-captioning-large_1+git-large-coco_1/test_images.jsonl",
+        "submission_path": "retriever/submission/TF-IDF_14.csv",
+        "log_path": "retriever/log/TF-IDF_14.log"
     }
     
     logging.basicConfig(
